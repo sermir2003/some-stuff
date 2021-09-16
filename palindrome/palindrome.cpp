9100a031ae62492efd445b1c436568a18769a700
Sergey Mironov <sdmironov@edu.hse.ru>
2021-09-16 12:45:50 +0300

    palindrome

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -3,6 +3,8 @@
 
 bool IsPalindrome(const std::string& str) {
     size_t right = str.length() - 1;
+    if (str[right] == 0)
+        --right;
     for (size_t left = 0; left < right; ++left) {
         if (!isspace(str[left])) {
             while (isspace(str[right])) {
