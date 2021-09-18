8017c6246434124aafa4c0a32f9fc339564db8d6
Sergey Mironov <sdmironov@edu.hse.ru>
2021-09-18 11:56:40 +0300

    associative_operation

--- associative_operation/associative_operation.cpp
+++ associative_operation/associative_operation.cpp
@@ -1,12 +1,15 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    size_t setSize = table.size();
-    for (size_t elem1 = 0; elem1 < setSize; ++elem1)
-        for (size_t elem2 = 0; elem2 < setSize; ++elem2)
-            for (size_t elem3 = 0; elem3 < setSize; ++elem3)
-                if (table[table[elem1][elem2]][elem3] !=
-                table[elem1][table[elem2][elem3]])
+    size_t set_size = table.size();
+    for (size_t elem1 = 0; elem1 < set_size; ++elem1) {
+        for (size_t elem2 = 0; elem2 < set_size; ++elem2) {
+            for (size_t elem3 = 0; elem3 < set_size; ++elem3) {
+                if (table[table[elem1][elem2]][elem3] != table[elem1][table[elem2][elem3]]) {
                     return false;
+                }
+            }
+        }
+    }
     return true;
 }
