52e6009067c7aa0cdfe75d7cd2e6fca86849cda0
Sergey Mironov <sdmironov@edu.hse.ru>
2021-09-18 12:02:37 +0300

    password

--- password/password.cpp
+++ password/password.cpp
@@ -3,26 +3,31 @@
 
 bool ValidatePassword(const std::string& password) {
     // first restriction
-    for (char elem : password)
-        if (elem < 33 || 126 < elem)
+    for (char elem : password) {
+        if (elem < 33 || 126 < elem) {
             return false;
+        }
+    }
     // second restriction
-    if (password.length() < 8 || password.length() > 14)
+    if (password.length() < 8 || password.length() > 14) {
         return false;
+    }
     // third restriction
-    bool upperExist = false;
-    bool lowerExist = false;
-    bool digitExist = false;
-    bool otherExist = false;
-    for (char elem : password)
-        if (isupper(elem))
-            upperExist = true;
-        else if (islower(elem))
-            lowerExist = true;
-        else if (isdigit(elem))
-            digitExist = true;
-        else
-            otherExist = true;
-    return int(upperExist) + int(lowerExist) +
-    int(digitExist) + int(otherExist) >= 3;
+    bool upper_exist = false;
+    bool lower_exist = false;
+    bool digit_exist = false;
+    bool other_exist = false;
+    for (char elem : password) {
+        if (isupper(elem)) {
+            upper_exist = true;
+        } else if (islower(elem)) {
+            lower_exist = true;
+        } else if (isdigit(elem)) {
+            digit_exist = true;
+        } else {
+            other_exist = true;
+        }
+    }
+    return static_cast<int>(upper_exist) + static_cast<int>(lower_exist) +
+               static_cast<int>(digit_exist) + static_cast<int>(other_exist) >= 3;
 }
