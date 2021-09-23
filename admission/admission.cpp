cc632094359007bde4a88935ab7f3e9d171a6a79
Sergey Mironov <sdmironov@edu.hse.ru>
2021-09-23 17:48:07 +0300

    admission

--- admission/admission.cpp
+++ admission/admission.cpp
@@ -1,17 +1,16 @@
 #include "admission.h"
-//#include <map>
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
     std::vector<const Applicant*> ordered_applicants(applicants.size(), nullptr);
     for (size_t i = 0; i < applicants.size(); ++i) {
         ordered_applicants[i] = &applicants[i];
     }
-    std::sort(ordered_applicants.begin(), ordered_applicants.end(), [] (const Applicant* const  ap1,
-                                                                       const Applicant* const ap2) {
-        return std::tuple(-ap1->points, ap1->student.birth_date.year, ap1->student.birth_date.month,
-                        ap1->student.birth_date.day, ap1->student.name) <
-               std::tuple(-ap2->points, ap2->student.birth_date.year, ap2->student.birth_date.month,
-                        ap2->student.birth_date.day, ap2->student.name);
+    std::sort(ordered_applicants.begin(), ordered_applicants.end(),
+              [](const Applicant* const  ap1, const Applicant* const ap2) {
+                  return std::tuple(-ap1->points, ap1->student.birth_date.year, ap1->student.birth_date.month,
+                                    ap1->student.birth_date.day, ap1->student.name) <
+                         std::tuple(-ap2->points, ap2->student.birth_date.year, ap2->student.birth_date.month,
+                                    ap2->student.birth_date.day, ap2->student.name);
     });
 
     std::unordered_map<std::string, size_t> cnt_place_for_university;
@@ -31,7 +30,7 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
     }
 
     for (auto it = admission_table.begin(); it != admission_table.end(); ++it) {
-        std::sort(it->second.begin(), it->second.end(), [] (const Student* const st1, const Student* const st2) {
+        std::sort(it->second.begin(), it->second.end(), [](const Student* const st1, const Student* const st2) {
             return std::make_tuple(st1->name, st1->birth_date.year, st1->birth_date.month, st1->birth_date.day) <
                    std::make_tuple(st2->name, st2->birth_date.year, st2->birth_date.month, st2->birth_date.day);
         });
